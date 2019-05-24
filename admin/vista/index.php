fccd1483916fb59fb0d46d338159a273bdb42fc2
Marco Antonio Cobos Farfan <marcocobos15@gmail.com>
2019-05-24 17:08:51 -0500

    Interciclo-C

--- admin/vista/index.php
+++ admin/vista/index.php
@@ -1,18 +1,39 @@
-<!DOCTYPE html>
+<?php
+session_start();
+$codigoui = $_SESSION['cod'];
+$nombresui = explode(" ", $_SESSION['nom']);
+$apellidosui =  explode(" ", $_SESSION['ape']);
+$correoui = $_SESSION['cor'];
+$usurol = $_SESSION['rol'];
+if (!isset($_SESSION['isLogged']) || $_SESSION['isLogged'] === FALSE) {
+    header("Location: /SistemaDeGestion/public/vista/login.html");
+}
+if ($usurol == 'user'){
+	?>
+	<!DOCTYPE html>
+	<html>
 
-<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Home</title>
 
-<head>
+	</head>
 
+	<body>
+		<?php  
+		include '../../../config/conexionDB.php'
 
-</head>
+		?>
 
+    	<h1>ADMIN</h1>
+    	<h1>ADMIN</h1>
 
-<body>
+	</body>
 
-    <h1>ADMIN</h1>
-    <h1>ADMIN</h1>
+	</html>
 
-</body>
-
-</html>
+<?php
+} else {
+	header("Location: ../../../config/acceso.html")
+}
+?>
