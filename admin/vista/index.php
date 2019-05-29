3e25ff1ded000b6b8b9e75a2d0feb67d169162c7
MalkiYupanki <malkiyupanki12@hotmail.com>
2019-05-29 16:46:51 -0500

    MENUMALKI

--- admin/vista/index.php
+++ admin/vista/index.php
@@ -9,11 +9,16 @@
 	<body>
 		<?php
 		include '../../config/conexionDB.php'
-
 		?>
+		
+		<h1>ADMIN</h1>
+		<h1>ADMIN</h1>
 
-		<h1>ADMIN</h1>
-		<h1>ADMIN</h1>
+		<a href="crear_producto.html">CREAR PRODUCTO</a>
+		<a href="crear_sucursal.html">CREAR SUCURSAL</a>
+		<a href="listar_productos.php">VER PRODUCTOS</a>
+		<a href="listar_sucursal.php">VER SUCURSALES</a>
+
 
 	</body>
 
