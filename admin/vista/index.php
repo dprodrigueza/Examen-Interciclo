89e8da55b8bcce18fe938e7ad09867e513dc99b5
Marco Antonio Cobos Farfan <marcocobos15@gmail.com>
2019-05-29 14:35:32 -0500

    Update

--- admin/vista/index.php
+++ admin/vista/index.php
@@ -6,14 +6,11 @@ $apellidosui =  explode(" ", $_SESSION['ape']);
 $correoui = $_SESSION['cor'];
 $usurol = $_SESSION['rol'];
 if (!isset($_SESSION['isLogged']) || $_SESSION['isLogged'] === FALSE) {
-<<<<<<< HEAD
     header("Location: /Examen-Interciclo/public/vista/index.php");
-=======
 	header("Location: /SistemaDeGestion/public/vista/login.html");
->>>>>>> bc32f8aff626d58d9612dc3b0d32dcb84f53e9cd
 }
 if ($usurol == 'user') {
-	?>
+?>
 	<!DOCTYPE html>
 	<html>
 
@@ -34,17 +31,12 @@ if ($usurol == 'user') {
 
 	</body>
 
-	</html>
+</html>
 
-<<<<<<< HEAD
-  <?php
-} //else {
-	//header("Location: ../../config/acceso.html");
-//}
-=======
-<?php
-} else {
-	header("Location: ../../config/acceso.html");
-}
->>>>>>> bc32f8aff626d58d9612dc3b0d32dcb84f53e9cd
-?>
+
+
+<!--<?php
+	/*} else {
+		header("Location: ../../config/acceso.html");
+	}*/
+?>-->
