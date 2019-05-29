38df8b3ff206ea5bbc8fa274733cedf3e1d73dc5
Diego Paul Rodriguez Alvarado <diego.gibi.97@gmail.com>
2019-05-29 14:16:26 -0500

    Usuarios

--- admin/controladores/eliminarUsu.php
+++ admin/controladores/eliminarUsu.php
@@ -1,33 +1,15 @@
 <?php
         include('../../config/conexionDB.php');
 
-        $sql = "SELECT * from usuarios where usu_del = 'N';";
+        $sql = "UPDATE usuarios SET usu_del= 'Y' WHERE usu_mail= '$_GET[mail]';";
+        echo $sql;
         $result = $conn->query($sql);
 
 
 
 
-        if ($result->num_rows > 0) {
-
-
-            while ($row = $result->fetch_assoc()) {
-                echo "<tr>";
-                echo ("<td>" . $row["usu_nombre"] . "</td>");
-                echo ("<td>" . $row["usu_apellido"] . "</td>");
-                echo ("<td>" . $row["usu_mail"] . "</td>");
-                echo ("<td>" . $row["usu_rol"] . "</td>");
-                echo ("<td>" . $row["usu_rol"] . "</td>");
-                echo ("<td> <a href = ../controladores/eliminarUsu.php?mail=" . $row["usu_mail"] . "&adm=". $_GET['mail'] . ">ELIMINAR</a>" . " </td>");
-                echo ("<td> <a href=actualizarUsuario.php?mail=" . $row["usu_mail"] . "&adm=". $_GET['mail'] . ">Modificar</a> </td>");
-                echo ("</tr>");
-            }
-        } else {
-            echo "<tr>";
-            echo ("<td colspan='7'>No existen usuarios registrados </td>");
-            echo ("</tr>");
-        }
-
-
+    
+            header("Location: ../vista/listarUsuarios.php?mail=$_GET[adm]");
+        
 
         $conn->close();
-        ?>
